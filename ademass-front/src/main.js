0f3a4455333f2bca330977dc4fb5b6772fb1e999
Innavoj <innavojaruges2371974@gmail.com>
2024-02-08 15:53:59 -0500

    commit

--- ademass-front/src/main.js
+++ ademass-front/src/main.js
@@ -1,6 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+import { createPinia } from "pinia";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const pinia = createPinia();
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
+app.use(vuetify);
+app.mount("#app");
