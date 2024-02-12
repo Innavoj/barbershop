3c89960b8cce30b624ee96b2795693021d5a54a2
AlexGogodev3 <109472309+AlexGogodev3@users.noreply.github.com>
2024-02-12 20:04:18 +0100

    updates from main

--- ademass-front/src/App.vue
+++ ademass-front/src/App.vue
@@ -1,31 +1,77 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/footer">Footer</router-link>
-  </nav>
-  <router-view />
+
+  <div v-if="isLayoutA()">
+    <v-layout class="rounded rounded-md">
+      <v-app-bar class="bg-black" title="BarberShop">
+      <v-btn
+        prepend-icon="mdi-account"
+      >
+        <template v-slot:prepend>
+          <v-icon color="success"></v-icon>
+        </template>
+        Hola Usuario
+      </v-btn>
+      <v-btn
+        prepend-icon="mdi-power"
+      >
+        <template v-slot:prepend>
+          <v-icon color="success"></v-icon>
+        </template>
+        Cerrar Sesión
+      </v-btn>
+
+
+
+      </v-app-bar>
+      <Menu></Menu>
+      <v-main class="d-flex align-center justify-center">
+        <v-col>
+          <router-view />
+        </v-col>
+      </v-main>
+    </v-layout>
+  </div>
+  <div v-else>
+      <Login>
+        <template #main>
+          <router-view></router-view>
+        </template>
+      </Login>
+  </div>
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup lang="ts">
+import Menu from './layouts/default/Menu.vue'
+import { useRoute } from 'vue-router';
+import Login from './layouts/login/Login.vue'
+
+const isLayoutA = () => {
+  const route = useRoute();
+  return route.meta.requiredAuth;
+};
+
+
+</script>
+
+<style>
+:root {
+  --color-primary: white;
+  --color-secondary: black;
+  --color-tertiary: #ccc;
+  --aqua: #33b5bd;
+}
+
+.custom-link {
+  text-decoration: none;
+  color: var(--aqua);
 }
 
 nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  display: flex;
+  justify-content: space-between;
 }
+
+
 </style>
+
---
+++ ademass-front/src/layouts/login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-layout class="rounded rounded-md">
+
+      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+        <slot name="main"></slot>
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped></style>
