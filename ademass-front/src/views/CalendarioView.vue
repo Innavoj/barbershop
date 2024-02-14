c4cc1155229b95809aec3323c34117fe35a82a12
Innajov <innavojaruges2371974@gmail.com>
2024-02-14 09:33:34 -0500

    Modificado Calendario

--- ademass-front/src/views/CalendarioView.vue
+++ ademass-front/src/views/CalendarioView.vue
@@ -1,8 +1,10 @@
 <template>
+  <Menu></Menu>
   <Calendario />
 </template>
 
 <script setup>
+import Menu from "@/layouts/default/Menu.vue";
 import Calendario from "@/components/Calendario.vue";
 </script>
 
