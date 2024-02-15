bb4905d98d94c7c0561f0b1428aa226f27b9164e
Innajov <innavojaruges2371974@gmail.com>
2024-02-15 08:57:39 -0500

    git pull origin Main

--- ademass-front/src/App.vue
+++ ademass-front/src/App.vue
@@ -1,52 +1,53 @@
 <template>
-
   <div v-if="isLayoutA()">
     <v-layout class="rounded rounded-md">
-      <v-app-bar class="bg-black" title="BarberShop">
-      <v-btn
-        prepend-icon="mdi-account"
-      >
-        <template v-slot:prepend>
-          <v-icon color="success"></v-icon>
-        </template>
-        Hola Usuario
-      </v-btn>
-      <v-btn
-        prepend-icon="mdi-power"
-      >
-        <template v-slot:prepend>
-          <v-icon color="success"></v-icon>
-        </template>
-        Cerrar Sesión
-      </v-btn>
-      </v-app-bar>
-      <Menu></Menu>
+      <v-app-bar class="custom-bg-color" title="BarberShop">
+        <router-link class="router-link" to="/login" style="color: #fff;">
+          <v-btn prepend-icon="mdi mdi-login">
+            <template v-slot:prepend>
+              <v-icon color="primary"></v-icon>
+            </template>
+            Iniciar sesión
+          </v-btn>
+        </router-link>
+        <router-link class="router-link" to="/register" style="color: #fff;">
+          <v-btn prepend-icon="mdi mdi-account-plus">
+            <template v-slot:prepend>
+              <v-icon color="primary"></v-icon>
+            </template>
+            Regristrarse
+          </v-btn>
+        </router-link>
+    </v-app-bar>
+      <Menu v-if="!isLayoutA()" ></Menu>
       <v-main class="d-flex align-center justify-center">
-        <v-col>
+        <v-col class="pa-0">
           <router-view />
           <Footer/>
         </v-col>
       </v-main>
     </v-layout>
   </div>
-  <div v-else>
-      <Login>
-        <template #main>
-          <router-view></router-view>
-        </template>
-      </Login>
-  </div>
 
+  <div v-else>
+    <Login>
+      <template #main>
+        <router-view></router-view>
+      </template>
+    </Login>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Menu from './layouts/default/Menu.vue'
+
 import { useRoute } from 'vue-router';
-import Login from './layouts/login/Login.vue'
-import Footer from './components/Footer.vue'
+import Login from '@/components/login/Login.vue'
+import Footer from '@/components/layouts/Footer.vue'
+import Menu from './components/layouts/Menu.vue';
 
 const isLayoutA = () => {
   const route = useRoute();
+  console.log('route', route);
   return route.meta.requiredAuth;
 };
 
@@ -65,12 +66,16 @@ const isLayoutA = () => {
   text-decoration: none;
   color: var(--aqua);
 }
+.custom-bg-color {
+  background-color: rgb(80, 75, 75)!important;
+  color: white!important;
+}
 
 nav {
   display: flex;
   justify-content: space-between;
 }
 
-
 </style>
+
 
