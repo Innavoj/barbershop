937fdea94a37e30326dc2d1bd5e1730d0d405776
Innajov <innavojaruges2371974@gmail.com>
2024-02-21 14:13:38 -0500

    Arreglado3o el Footer y la Encuesta

--- ademass-front/src/views/EncuestaView.vue
+++ ademass-front/src/views/EncuestaView.vue
@@ -20,7 +20,9 @@
         <v-card-title>Experiencia general:</v-card-title>
         <v-divider></v-divider>
         <v-card-text>
-          ¿Cómo calificaría su experiencia general en la peluquería?
+          <strong
+            >¿Cómo calificaría su experiencia general en la peluquería?</strong
+          >
         </v-card-text>
         <v-radio-group v-model="calificacion" inline>
           <v-radio label="Excelente" value="Excelente"></v-radio>
@@ -29,7 +31,7 @@
           <v-radio label="Mala" value="Mala"></v-radio>
         </v-radio-group>
         <v-card-text>
-          ¿Con qué frecuencia visita nuestra peluquería?
+          <strong>¿Con qué frecuencia visita nuestra peluquería?</strong>
         </v-card-text>
         <v-col col="10" sm="6">
           <v-select
@@ -39,7 +41,7 @@
           ></v-select>
         </v-col>
         <v-card-text>
-          ¿Recomendaría nuestra peluquería a amigos/familiares?
+          <strong>¿Recomendaría nuestra peluquería a amigos/familiares?</strong>
         </v-card-text>
         <v-radio-group v-model="recomienda">
           <v-radio label="Si" value="Si"></v-radio>
@@ -54,29 +56,32 @@
       >
         <v-card-title>Servicios específicos:</v-card-title>
         <v-divider></v-divider>
-        <v-card-text> Calidad del corte/peinado. </v-card-text>
+        <v-card-text> <strong>Calidad del corte/peinado.</strong> </v-card-text>
         <v-radio-group v-model="calidad" inline>
           <v-radio label="Excelente" value="Excelente"></v-radio>
           <v-radio label="Muy Buena" value="MBuena"></v-radio>
           <v-radio label="Buena" value="Buena"></v-radio>
           <v-radio label="Mala" value="Mala"></v-radio>
         </v-radio-group>
-        <v-card-text> Tiempo de espera. </v-card-text>
+        <v-card-text> <strong>Tiempo de espera.</strong> </v-card-text>
         <v-col col="10" sm="6">
           <v-text-field
-            prepend-icon="mdi-timer"
             type="time"
             v-model="tiempo"
             label="Tiempo"
           ></v-text-field>
         </v-col>
-        <v-card-text> Amabilidad y profesionalismo del personal. </v-card-text>
+        <v-card-text>
+          <strong>Amabilidad y profesionalismo del personal.</strong>
+        </v-card-text>
         <v-radio-group v-model="amable" inline>
           <v-radio label="Amable - Profesional" value="AP"></v-radio>
           <v-radio label="Amable - No Profesional" value="ANP"></v-radio>
           <v-radio label="Ingrato" value="Ingrato"></v-radio>
         </v-radio-group>
-        <v-card-text> Limpieza e higiene del salón. </v-card-text>
+        <v-card-text>
+          <strong>Limpieza e higiene del salón.</strong>
+        </v-card-text>
         <v-radio-group v-model="limpieza" inline>
           <v-radio label="Excelente" value="Excelente"></v-radio>
           <v-radio label="Muy Buena" value="MBuena"></v-radio>
@@ -93,14 +98,16 @@
         <v-card-title>Comunicación:</v-card-title>
         <v-divider></v-divider>
         <v-card-text>
-          ¿El peluquero/a comprendió sus preferencias?
+          <strong>¿El peluquero/a comprendió sus preferencias?</strong>
         </v-card-text>
         <v-radio-group v-model="preferencias" inline>
           <v-radio label="Si" value="Si"></v-radio>
           <v-radio label="No" value="No"></v-radio>
         </v-radio-group>
         <v-card-text>
-          ¿Hubo una comunicación clara sobre los servicios y precios?
+          <strong
+            >¿Hubo una comunicación clara sobre los servicios y precios?</strong
+          >
         </v-card-text>
         <v-radio-group v-model="comunicacion" inline>
           <v-radio label="Si" value="Si"></v-radio>
@@ -115,13 +122,15 @@
       >
         <v-card-title>Atención al cliente:</v-card-title>
         <v-divider></v-divider>
-        <v-card-text> ¿Recibió una cálida bienvenida al llegar? </v-card-text>
+        <v-card-text>
+          <strong>¿Recibió una cálida bienvenida al llegar?</strong>
+        </v-card-text>
         <v-radio-group v-model="bienvenida" inline>
           <v-radio label="Si" value="Si"></v-radio>
           <v-radio label="No" value="No"></v-radio>
         </v-radio-group>
         <v-card-text>
-          ¿El personal fue atento y cortés durante su visita?
+          <strong>¿El personal fue atento y cortés durante su visita?</strong>
         </v-card-text>
         <v-radio-group v-model="atento" inline>
           <v-radio label="Si" value="Si"></v-radio>
@@ -136,13 +145,15 @@
       >
         <v-card-title>Satisfacción con el resultado:</v-card-title>
         <v-divider></v-divider>
-        <v-card-text> ¿Está satisfecho con su corte/peinado? </v-card-text>
+        <v-card-text>
+          <strong>¿Está satisfecho con su corte/peinado?</strong>
+        </v-card-text>
         <v-radio-group v-model="satisfecho" inline>
           <v-radio label="Satisfecho" value="Satisfecho"></v-radio>
           <v-radio label="No Satisfecho" value="NoSatisfecho"></v-radio>
         </v-radio-group>
         <v-card-text>
-          ¿El resultado final cumplió con sus expectativas?
+          <strong>¿El resultado final cumplió con sus expectativas?</strong>
         </v-card-text>
         <v-col col="12" sm="11">
           <v-textarea
@@ -161,7 +172,7 @@
       >
         <v-card-title>Comentarios adicionales:</v-card-title>
         <v-divider></v-divider>
-        <v-card-text> Comentarios: </v-card-text>
+        <v-card-text> <strong>Comentarios</strong> </v-card-text>
         <v-col col="12" sm="11">
           <v-textarea
             prepend-icon="mdi-comment"
@@ -171,7 +182,7 @@
             variant="outlined"
           ></v-textarea>
         </v-col>
-        <v-card-text> Sugerencias: </v-card-text>
+        <v-card-text> <strong>Sugerencias</strong> </v-card-text>
         <v-col col="12" sm="11">
           <v-textarea
             prepend-icon="mdi-comment"
@@ -185,7 +196,7 @@
       <v-divider></v-divider>
       <v-tooltip :location="location" :origin="origin" no-click-animation>
         <template v-slot:activator="{ props }">
-          <v-btn v-bind="props" class="ma-2" color="success"
+          <v-btn v-bind="props" class="ma-2" color="cyan"
             >Aceptar <v-icon end icon="mdi-checkbox-marked-circle"></v-icon
           ></v-btn>
           <v-btn class="ma-2">
