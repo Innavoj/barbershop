b829c45d8bf998314bca994c20247d443fa237b4
Mariana <marianasmatos1@gmail.com>
2024-02-23 22:57:57 -0500

    corrigiendo loader de valoraciones

--- ademass-front/src/views/ValoracionesView.vue
+++ ademass-front/src/views/ValoracionesView.vue
@@ -1,50 +1,67 @@
 <template>
     <v-app class="w-100 d-flex flex-column align-center text-center pa-2 animation-opacity">
         <h1 class="text-h4 mb-8 my-12 title-h1">Valoraciones de nuestros servicios</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas.</p>
-        <v-sheet class="w-100 my-12">
-            
-            <div class="text-center">
-                <pre>{{ rating }}</pre>
-                <v-rating
-                v-model="rating"
-                hover
-                half-increments
-                color="#ccc"
-                active-color="#33b5bd"
-                ></v-rating>
-            </div>
+        <small class>¡Gracias por compartir tu opinión con nosotros!</small>
+        <small class>Tu opinión nos impulsa a seguir mejorando.</small>
+        <small class><b>Nuestros usuarios nos dan:</b></small>
+        <div class="text-h2 mt-5">
+                    3.5
+                <span class="text-h6 ml-n3">/5</span>
+        </div>
+        <v-rating
+        v-model="ratingFetch"
+        readonly
+        color="yellow-darken-3"
+        class="justify-center"
+        ></v-rating>
+        <v-card
+      class="w-100 my-12"
+        >
+        <p class="w-100 my-8">¿Qué tan satisfecho estás de nuestro servicio?.</p>
 
-            <v-form class="w-100 p-3">
-                <v-progress-linear
-                :active="isUpdating"
-                :indeterminate="isUpdating"
-                absolute
-                bottom
-                color="#33b5bd"
-                ></v-progress-linear>
-                <v-container>
+        <div class="text-center my-4">
+            <pre>{{ rating }}</pre>
+            <v-rating
+            v-model="rating"
+            hover
+            half-increments
+            color="#ccc"
+            active-color="#33b5bd"
+            ></v-rating>
+        </div>
+        <v-divider inset></v-divider>
+        <v-form class="w-100 p-3">
+            <v-progress-linear
+            :active="isUpdating"
+            :indeterminate="isUpdating"
+            absolute
+            bottom
+            color="#33b5bd"
+            ></v-progress-linear>
+            <v-container>
                 <v-row dense>
                     <v-col cols="12">
                         <v-text-field
                             v-model="title"
                             :disabled="isUpdating"
                             color="#000"
-                            label="Email"
+                            label='Email'
                             variant="underlined"
                         ></v-text-field>
                     </v-col>
+        
                     <v-col cols="12" class="my-4">
-                        <v-textarea label="Mensaje" variant="outlined" v-model="comments"></v-textarea>
+                        <v-textarea label="Comentarios" variant="outlined" v-model="comments"></v-textarea>
                     </v-col>
                     <v-col cols="12" class="my-4">
                         <v-btn color="#33b5bd" @click="handleSend">Enviar</v-btn>
                     </v-col>
                 </v-row>
-                </v-container>
-            </v-form>
-
-            <div class="d-flex justify-start my-12 text-h5 ">
+            </v-container>
+        </v-form>
+        
+    </v-card>
+    <div class="d-flex justify-start my-12 text-h5 ">
             Opiniones
             </div>
 
@@ -62,7 +79,7 @@
                             color="grey-darken-3"
                             :image="el.avatar"
                         ></v-avatar>
-                        <v-list-item-title>{{el.usuario}}</v-list-item-title>
+                        <v-list-item-title>{{el.Usuario}}</v-list-item-title>
                     </div>
                     <v-rating
                     v-model="el.estrellas"
@@ -123,8 +140,6 @@
                     </template>
                 </v-list-item>
             </v-list>
-
-        </v-sheet>
     </v-app>
   </template>
   <script lang="ts" setup>
@@ -142,14 +157,14 @@
 
     /* Select ranting */
 
-    const rating = ref(3.5)
+    const rating = ref(4.5)
     const ratingFetch = ref(3.5)
 
     /* Comments table */
     const array = [
-        {usuario: 'blabla', comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas', estrellas: 5, avatar: "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"},
-        {usuario: 'blabla', comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas', estrellas: 1, avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'},
-        {usuario: 'blabla', comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas', estrellas: 2.5, avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'},
-        {usuario: 'blabla', comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas', estrellas: 3.5, avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'},
+        {Usuario: 'Usuario', comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas', estrellas: 5, avatar: "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"},
+        {Usuario: 'Usuario', comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas', estrellas: 1, avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'},
+        {Usuario: 'Usuario', comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas', estrellas: 2.5, avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'},
+        {Usuario: 'Usuario', comentario: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptas', estrellas: 3.5, avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'},
     ]
 </script>
